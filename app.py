63296835759f2dccae300efe4d6fde2b4212f989
Edward Olmos <edwardolmos3@gmail.com>
2021-07-09 15:06:05 -0700

    create python script and test data

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from os import path
+import csv
+
+data = []
+
+with open('data.csv', newline='') as csvfile:
+  reader = csv.reader(csvfile)
+  for row in reader:
+    data.append(row[0])
+
+dataLength = len(data)
+
+if (path.exists('script.sql')):
+  os.remove("script.sql")
+
+with open("script.sql", "a") as scriptfile:
+  scriptfile.write('INSERT INTO `tableName` \n')
+  scriptfile.write('VALUES \n')
+
+  for i in range(0, dataLength):
+    keyValuePair = data[i].split('=')
+    scriptfile.write("('" + keyValuePair[0] + "','" + keyValuePair[1] + "'),\n")
+
+  keyValuePair = data[dataLength-1].split('=')
+  scriptfile.write("('" + keyValuePair[0] + "','" + keyValuePair[1] + "');\n")
