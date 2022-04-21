7fbc46ca08e3dc69806a21868783ac0b547226c2
Edward Olmos <edwardolmos3@gmail.com>
2022-04-21 12:33:58 -0700

    added array exercises

---
+++ data-structures.py
@@ -0,0 +1,26 @@
+"""Arrays - AKA 'Lists'"""
+array_a = [1, 2, 3]
+print(array_a[-1])
+
+# slicing - left INCLUSIVE, right EXCLUSIVE
+array_a = [0, 10, 20, 30, 40, 50]
+print(array_a[2:5])
+print(array_a[1:])
+print(array_a[:3])
+
+for i, num in enumerate(array_a):
+    print(i, num)
+
+# Linked List
+
+# Stack
+
+# Queue
+
+# Hash Table
+
+# Hash Set
+
+# Tree
+
+# Priority Queue and Graph
